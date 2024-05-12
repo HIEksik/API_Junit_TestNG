65cacba855ab721295afb09b5e7c12a924da4671
halil <h.i.eksik@hotmail.com>
2024-05-12 21:29:40 +0300

    api

--- src/test/java/tests/C28_PojoClassPOST.java
+++ src/test/java/tests/C28_PojoClassPOST.java
@@ -1,6 +1,18 @@
 package tests;
 
-public class C28_PojoClassPOST {
+import baseUrl.BaseUrlHerOkuApp;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.Assert;
+import org.junit.Test;
+import pojos.PojoHerOkuAppBookingdates;
+import pojos.PojoHerOkuAppExpBody;
+import pojos.PojoHerOkuAppReqBody;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
+
+public class C28_PojoClassPOST extends BaseUrlHerOkuApp {
 
     /*
     https://restful-booker.herokuapp.com/booking url’ine
@@ -35,6 +47,39 @@ public class C28_PojoClassPOST {
                         "additionalneeds":"wi-fi"
                     }
      */
+
+    @Test
+    public void test01(){
+
+
+        specHerOkuApp.pathParam("pp1","booking");
+
+        PojoHerOkuAppBookingdates bookingdatesPojo=new PojoHerOkuAppBookingdates("2021-06-01","2021-06-10");
+
+        PojoHerOkuAppReqBody reqBodyPojo=new PojoHerOkuAppReqBody("Ahmet","Bulut",500,false,bookingdatesPojo,"wi-fi");
+
+
+        PojoHerOkuAppReqBody booking=new PojoHerOkuAppReqBody("Ahmet","Bulut",500,false,bookingdatesPojo,"wi-fi");
+        PojoHerOkuAppExpBody expBodyPojo=new PojoHerOkuAppExpBody(1088,booking);
+
+        //PojoHerOkuAppExpBody expBodyPojo=new PojoHerOkuAppExpBody(2565,reqBodyPojo); // request bodyle response expected aynıysa bu şekilde olur
+
+        Response response=given().spec(specHerOkuApp).contentType(ContentType.JSON).when().body(reqBodyPojo).post("/{pp1}");
+
+        PojoHerOkuAppExpBody resPojo=response.as(PojoHerOkuAppExpBody.class);
+
+        assertEquals(expBodyPojo.getBookingid(),resPojo.getBookingid());
+        assertEquals(expBodyPojo.getBooking().getFirstname(),resPojo.getBooking().getFirstname());
+        assertEquals(expBodyPojo.getBooking().getLastname(),resPojo.getBooking().getLastname());
+        assertEquals(expBodyPojo.getBooking().getTotalprice(),resPojo.getBooking().getTotalprice());
+        assertEquals(expBodyPojo.getBooking().isDepositpaid(),resPojo.getBooking().isDepositpaid());
+        assertEquals(expBodyPojo.getBooking().getBookingdates().getCheckin(),resPojo.getBooking().getBookingdates().getCheckin());
+        assertEquals(expBodyPojo.getBooking().getBookingdates().getCheckout(),resPojo.getBooking().getBookingdates().getCheckout());
+        assertEquals(expBodyPojo.getBooking().getAdditionalneeds(),resPojo.getBooking().getAdditionalneeds());
+
+
+
+    }
 
 
 }
